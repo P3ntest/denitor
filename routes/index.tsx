eb21c009bd72efe8be7decff6cbb9064dece3155
P3ntest <p3ntest@gmx.de>
2022-06-30 12:24:12 +0200

    updates to apps page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,11 @@
 /** @jsx h */
 import { h } from "preact";
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { Todo } from "../utils/db.ts";
+import { Application } from "../utils/db.ts";
+import { tw } from "../utils/twind.ts";
 
 interface LoaderData {
-  todos: Todo[];
+  apps: Application[];
 }
 
 export const handler: Handlers = {
@@ -12,26 +13,31 @@ export const handler: Handlers = {
     const formData = await req.formData();
     const name = formData.get("name")! as string;
 
-    await Todo.create({ name });
+    await Application.create({
+      name,
+      token: crypto.randomUUID(),
+    });
 
     return Response.redirect(req.url, 302);
   },
   async GET(req, ctx) {
-    const todos = await Todo.all();
+    const apps = await Application.all();
 
-    return ctx.render({ todos });
+    return ctx.render({ apps });
   },
 };
 
 export default function AboutPage({ data }: PageProps<LoaderData>) {
   return (
     <main>
-      {JSON.stringify(data)}
-      <ul>
-        {data.todos.map((todo: Todo) => (
-          <li>{todo.name}</li>
+      <h1 class={tw`text-xl font-bold`}>All Apps</h1>
+      <div>
+        {data.apps.map((app: Application) => (
+          <div class={tw`bg-gray-100 rounded border-2 shadow p-2 m-2`}>
+            <h1 class={tw`text-lg`}>{app.name}</h1>
+          </div>
         ))}
-      </ul>
+      </div>
       <form method="post">
         <input type="text" name="name" />
         <input type="submit" value="Submit" />
--- utils/db.ts
+++ utils/db.ts
@@ -5,6 +5,8 @@ import {
   PostgresConnector,
 } from "https://deno.land/x/denodb/mod.ts";
 
+import "https://deno.land/x/dotenv/load.ts";
+
 const connection = new PostgresConnector({
   host: Deno.env.get("DB_HOST")!,
   username: Deno.env.get("DB_USER")!,
@@ -14,16 +16,17 @@ const connection = new PostgresConnector({
 
 const db = new Database(connection);
 
-export class Todo extends Model {
-  static table = "todos";
+export class Application extends Model {
+  static table = "apps";
   static timestamps = true;
 
   static fields = {
     id: { primaryKey: true, autoIncrement: true },
     name: DataTypes.STRING,
+    token: DataTypes.STRING,
   };
 }
 
-db.link([Todo]);
+db.link([Application]);
 
-await db.sync({ drop: true });
+await db.sync();
